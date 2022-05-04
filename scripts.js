fdb6221963bcb7c7a89f6d682534b5b4aab0d7f6
Raul <raulvillajr95@gmail.com>
2022-05-04 13:57:28 -0700

    Add Start, Pause, and Reset Buttons

--- scripts.js
+++ scripts.js
@@ -1,15 +1,35 @@
-const mainTimer = document.getElementById('main-timer')
-const btnStart = document.getElementById('btn-start')
+const mainTimer = document.getElementById('main-timer');
+const btnStart = document.getElementById('btn-start');
+const btnPause = document.getElementById('btn-pause');
+const btnReset = document.getElementById('btn-reset');
 
-let count = 0
+let seconds = 0;
+let intervalID;
 
+let running = false;
 btnStart.addEventListener('click', function() {
-  setInterval(function() {
-    count += 1
-    mainTimer.textContent = count
-  }, 1000)
-})
+  if (running == false) {
+    intervalID = setInterval(function() {
+      running = true;
+      seconds += 1;
+      mainTimer.textContent = seconds;
+      console.log(seconds);
+      console.log(running);
+    }, 1000);
+  };
+});
 
+btnPause.addEventListener('click', function() {
+  running = false;
+  clearInterval(intervalID);
+});
+
+btnReset.addEventListener('click', function() {
+  running = false;
+  clearInterval(intervalID);
+  seconds = 0;
+  mainTimer.textContent = seconds;
+})
 
 
 
@@ -17,10 +37,8 @@ btnStart.addEventListener('click', function() {
 Creating a super super basic timer
 
 ideas
--just seconds forever
--start
--pause
--reset
+-join start/pause into 1 button
+-make sure everything is aligned well
 -mins and seconds
 -countdown?? manually inputed on js
 -instead of manual input, use session
